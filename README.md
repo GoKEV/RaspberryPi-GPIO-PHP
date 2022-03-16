f68cb2f1b704dbb7652a85738a97c88d96382a8b
Kevin Holmes <kev@gokev.com>
2022-03-16 18:18:19 -0500

    momentary switch capabilities

--- README.md
+++ README.md
@@ -57,9 +57,17 @@ pi@raspberrypi:~ $
 ![RaspberryPiPinout.png](files/RaspberryPiPinout.png?raw=true "RaspberryPiPinout.png")
 
 
+## WiringPi deprecation - don't email Gordon to support this.
 
-## Author
-
-This project was created in 2019 by [Kevin Holmes](http://GoKEV.com/).
+After reading Gordon's self-proclaimed rant http://wiringpi.com/wiringpi-deprecated/ I pulled the source into this repo and decided to distribute the last version I could find.  I am doing my best to honor Gordon and GPL -- I didn't remove anything as far as credit, contact info etc.  Please don't call Gordon for support.  Read his rant and you'll understand why.  WiringPi is awesome and I hope everyone is happy with the way I've integrated it here.
 
 
+## Project origin and major milestones:
+
+This project was created in 2016 by [Kevin Holmes](http://GoKEV.com/), based on the desire for a simple GPIO interface pane.  It has evolved slightly to adapt for platform changes, but is still the same basic interface it started out to be.
+
+- 2022-03-17  I added the functionality for momentary buttons.  They can be configured in the index.php as latching or momentary with a user-defined delay in seconds.  The blue LED doesn't change from the default value.  I'll fix that some day.
+- 2022-03-16  Read above, "WiringPi deprecation - don't email Gordon to support this."
+- 2019-03-29  Ansible playbook installer allows complete automated setup with a new Pi
+- 2019-01-01  Initial working interface with GPIO
+- 2016-04-03  Initial PHP button interface
--- files/html/index.php
+++ files/html/index.php
@@ -17,35 +17,87 @@ error_reporting( error_reporting() & ~E_NOTICE );
 	aside from apache2 and php5 -- using Raspberry Pi Model B+ (the 2012 model)
 */
 
-$devices = array(
-	'Button One [11]' => '0',
-	'Button Two [12]' => '1',
-	'Button Three [13]' => '2',
-	'Button Four [15]' => '3',
-	'Button Five [16]' => '4',
-	'Button Six [18]' => '5',
-	'Button Seven [22]' => '6',
-	'Button Eight [7]' => '7',
-);
+//  The new TYPE field indicates behavior
+//	L0 / L1 = latching switch (the 0 or 1 makes no difference)
+//	M05 / M15 = momentary switch with a default value of 0 or 1, 5 second delay
+//		A switch with M05 will be OFF... then turn on for 5seconds, then off
+//		A switch with M13 will be ON... then turn of for 3seconds, then on
+
+$devices[0][name]="Button One";
+$devices[0][type]="L0";
+$devices[0][pin]=11;
+
+$devices[1][name]="Button Two";
+$devices[1][type]="L1";
+$devices[1][pin]=12;
+
+$devices[2][name]="Button Three (Momentary 3 seconds, normally off)";
+$devices[2][type]="M03";
+$devices[2][pin]=13;
+
+$devices[3][name]="Button Four (Momentary 5 seconds, normally on)";
+$devices[3][type]="M15";
+$devices[3][pin]=15;
 
 /*
 	This is your array of devices.  See a GPIO pinout to make sure you're 
 	using the correct wires for GPIO 0,1,2,3 or change these to whatever
-	values you use. 
+	values you use.
 
-	The text portion is simply for display.  Change it to what you want
-	displayed on the web interface.  Adding additional elements to this array
-	will create more buttons (provided a unique name is used).  I tested this
-	with TWO GPIO relays and added in values for GPIO 10,11,12,and 13.
+	The NAME portion is simply for display.  Change it to what 
+	you want displayed on the web interface.  Adding additional
+	elements to this array will create more buttons. I tested
+	this with TWO GPIO relays and had eight GPIO addresses going.
+
+EXAMPLES FOR ADDING GPIO DEVICES:
+
+	This will add a device for the non-existent GPIO 40
+	which would be wired into non-existent pin 45
+	and a momentary contact, waiting 999 seconds before
+	returning
+$devices[40][name]="Some fake button that does nothing";
+$devices[40][type]="M1999";
+$devices[40][pin]=45;
+
+LETs BREAK THIS DOWN:
+$devices[40][type]="M1999";
+
+M 1 999
+| |  |___________The delay of a momentary push (from 0 to 999 seconds)
+| |__________The second character is the default state. 0off 1on
+|________The first letter M says "momentary".  L for "latching"
+
+This value is parsed using substr, so delay is always 3rd (and 4th and 5th) character)
 
 */
 
+
+
+
+
+
+
+
+
+// We get the input string from the URL request.
 $io = preg_replace("/[^0-9]/","",$_REQUEST['io']);	// SECURITY:  strip out anything in the io var that isn't a number
 
 
 // Listen for the input string to decide what we do
 switch ($_REQUEST['op']) {
 	case "status":
+		$content = check_the_status($io);
+		break;
+	case "moff":
+		$type = preg_replace("/[^LM0-9]/","",$_REQUEST['type']);
+		$delay = substr($type, 2, 3);
+		curl_command($io,"write", "1", $delay);
+		$content = check_the_status($io);
+		break;
+	case "mon":
+		$type = preg_replace("/[^LM0-9]/","",$_REQUEST['type']);
+		$delay = substr($type, 2, 3);
+		curl_command($io,"write", "0", $delay);
 		$content = check_the_status($io);
 		break;
 	case "off":
@@ -61,10 +113,9 @@ switch ($_REQUEST['op']) {
 		break;
 	default:
 		init_gpio($devices);
-		$content = get_list($devices);
+		$content .= get_list($devices);
 		$content .= show_gpio_graphic();
-
-// Comment out the show_gpio_graphic line above to hid the graphic from the web page
+// Comment out the show_gpio_graphic line above to hide the graphic from the web page
 		break;
 }
 
@@ -86,15 +137,24 @@ switch ($_REQUEST['op']) {
 </body>
 
 
-<?
+<?php
 
 ////////////////////////////////////////////////////////////////////////////////
 
 function check_the_status($io){
-	$onurl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&op=on";
-	$offurl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&op=off";
+        global $devices;
+	$type = $devices[$io][type];
 	$status = curl_command($io);
-	$led = ($status ? "off.png" : "on.png");
+	if ( preg_match("/^M/",$type)){
+		$onurl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&type=" . $type . "&op=mon";
+		$offurl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&type=" . $type . "&op=moff";
+		$led = ($status ? "blue_off.png" : "blue_on.png");
+	}else{
+		$onurl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&type=" . $type . "&op=on";
+		$offurl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&type=" . $type . "&op=off";
+		$led = ($status ? "red_off.png" : "red_on.png");
+	}
+
 	$link = ($status ? $offurl : $onurl);
 	return led_link($led,$link);
 
@@ -116,8 +176,16 @@ ALLDONE;
 
 function init_gpio($devices){
 	$out = null;
-	foreach ($devices as $name => $io){
+	foreach ($devices as $io => $info){
+		$name = $info[name];
+		$type = $info[type];
+
 		curl_command($io,"mode","out");
+
+		if ( preg_match("/^M/",$type)){
+			$newval = substr($type, 1, 1); 
+			curl_command($io,"write", $newval);
+		}
 	}
 }
 
@@ -125,13 +193,16 @@ function init_gpio($devices){
 
 function get_list($devices){
 	$out = null;
-	foreach ($devices as $name => $io){
+	foreach ($devices as $io => $info){
+		$name = $info[name];
+		$pin = $info[pin];
+		$type = $info[type];
 		$cell = make_cell($io);
 		$out .=<<<ALLDONE
 	<tr>
 		<td>
-			$name 
-			<div style="font-family: Verdana, Arial, Sans; font-size: 8px">	(IO $io)</div>
+			$name
+			<div style="font-family: Verdana, Arial, Sans; font-size: 8px">	(IO $io / PIN $pin)</div>
 		</td>
 		<td>
 			$cell
@@ -147,14 +218,36 @@ ALLDONE;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-function curl_command($io,$action="read",$value=""){
-	$command =<<<ALLDONE
+function curl_command($io,$action="read",$value="",$delay=""){
+
+	if ($delay > 0){
+	        $newval = ($value == 1 ? 0 : 1);
+		$command =<<<ALLDONE
+         gpio $action $io $value ; sleep $delay ; gpio $action $io $newval
+ALLDONE;
+	}else{
+
+		$command =<<<ALLDONE
          gpio $action $io $value
 ALLDONE;
-
+}
 	$reply = shell_exec($command);
 	$result = preg_replace("/\s+/","",$reply);
 	return ($result ? false : true);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function make_cell($io){
+	global $devices;
+	$type = $devices[$io][type];
+	$sturl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&type=" . $type . "&op=status";
+
+	$out =<<<ALLDONE
+<iframe frameborder="0" width="75" height="75" align="center" valign="mid" src="$sturl"></iframe>
+ALLDONE;
+	return $out;
 
 }
 
@@ -198,17 +291,5 @@ function show_gpio_graphic(){
 
 ALLDONE;
 	return $out;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-function make_cell($io){
-	$sturl = $_SERVER['SCRIPT_NAME'] . "?io=" . $io . "&op=status";
-
-	$out =<<<ALLDONE
-<iframe frameborder="0" width="75" height="75" align="center" valign="mid" src="$sturl"></iframe>
-ALLDONE;
-	return $out;
-
 }
 
